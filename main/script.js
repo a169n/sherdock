e21cc0f5c1cb6f2e6a8ccc09dfd6cea2da53ebf4
Aibyn <a1byn.talgat05@gmail.com>
2023-10-19 16:38:38 +0500

    added countdown timer

--- main/script.js
+++ main/script.js
@@ -33,3 +33,28 @@ form.addEventListener("submit", function (event) {
 
      form.reset();
 });
+
+const discountEndDate = new Date('2023-10-22T23:59:59');
+
+
+function updateCountdown() {
+     const currentDate = new Date();
+     const timeLeft = discountEndDate - currentDate;
+
+     if (timeLeft < 0) {
+          document.getElementById("countdown").innerHTML = "Discount Expired";
+     } else {
+          const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+          const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+          const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+          const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+          document.getElementById("days").textContent = String(days).padStart(2, '0');
+          document.getElementById("hours").textContent = String(hours).padStart(2, '0');
+          document.getElementById("minutes").textContent = String(minutes).padStart(2, '0');
+          document.getElementById("seconds").textContent = String(seconds).padStart(2, '0');
+     }
+}
+
+updateCountdown();
+setInterval(updateCountdown, 1000);
