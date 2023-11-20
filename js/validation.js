1af3cfc992d1a78b009cde5fa3693bf3eabbe4f6
Aibyn <a1byn.talgat05@gmail.com>
2023-11-20 21:08:21 +0500

    now form data displayed on admin panel

--- js/validation.js
+++ js/validation.js
@@ -18,20 +18,51 @@ function validateForm() {
      let reasonForHospitalization = document.getElementById("reasonForHospitalization");
      let admissionDate = document.getElementById("admissionDate");
      let contactNumber = document.getElementById("contactNumber");
+     let roomPreference = document.getElementById("roomPreference");
+
      let errorMessage = document.getElementById("errorMessage");
      let errorMessageBtn = document.getElementById("closeError");
 
-     if (patientName.value === "" || patientAge.value === "" || reasonForHospitalization.value === "" || admissionDate.value === "" || contactNumber.value === "") {
+     if (patientName.value === "" || patientAge.value === "" || reasonForHospitalization.value === "" || admissionDate.value === "" || contactNumber.value === "" || roomPreference.value === "undefined") {
           errorMessage.style.display = "block";
           errorMessageBtn.style.display = "block";
 
           scrollErrorMessageToCenter();
      } else {
-          document.getElementById("myForm").reset();
-          alert("Форма успешно отправлена!");
+          // Create an object to store the form data
+          const formData = {
+               patientName: patientName.value,
+               patientAge: patientAge.value,
+               reasonForHospitalization: reasonForHospitalization.value,
+               admissionDate: admissionDate.value,
+               contactNumber: contactNumber.value,
+               roomPreference: roomPreference.value
+          };
 
+          // Send the data to localhost
+
+          // Save the data locally
+          saveFormDataToLocal(formData);
+
+          // Reset the form
+          document.getElementById("myForm").reset();
+
+          alert("Form submitted successfully!");
           document.getElementById("submissionResult").textContent = "";
      }
+}
+
+
+
+function saveFormDataToLocal(data) {
+     // Retrieve existing data from localStorage
+     const existingData = JSON.parse(localStorage.getItem('formData')) || [];
+
+     // Add new data to the array
+     existingData.push(data);
+
+     // Save the updated data back to localStorage
+     localStorage.setItem('formData', JSON.stringify(existingData));
 }
 
 document.addEventListener("DOMContentLoaded", function () {
